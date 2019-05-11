24461dadc9b4b7ccfaea4d350ce67440ac5b1c3a
ZuoXinyu <iamzuoxinyu@gmail.com>
2019-05-11 14:47:31 +0800

    polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,7 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+mpd ~/.config/mpd/mpd.conf &
+
 # Launch bar1 and bar2
-polybar top
+polybar example &
 
 echo "Bars launched..."
