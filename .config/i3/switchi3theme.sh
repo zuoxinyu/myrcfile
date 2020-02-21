0c7124b5393f81244bfe18414d574aa587986408
ZuoXinyu <iamzuoxinyu@gmail.com>
2020-02-21 17:39:40 +0800

    improve i3 switch thmeme script

--- .config/i3/switchi3theme.sh
+++ .config/i3/switchi3theme.sh
@@ -3,25 +3,75 @@ LIGHT='light'
 DARK='dark'
 THEME_FILE=~/.config/i3/current_theme
 
-current=`cat ${THEME_FILE}`
+current_mode=`cat $THEME_FILE`
+cmd=$1
 
-if [ "$current" = "$DARK" ];then
-    ln -sf ~/.config/polybar/colors.light.ini ~/.config/polybar/colors.ini
-    ln -sf ~/.config/dunst/dunstrc.light ~/.config/dunst/dunstrc
-    ln -sf ~/.config/rofi/themes/colors.light.rasi ~/.config/rofi/themes/colors.rasi
+switch-theme() {
+    mode=$1
+    echo $mode > $THEME_FILE
+    # TODO: pipefail rollback
+    ln -sf ~/.config/rofi/themes/colors.$mode.rasi  ~/.config/rofi/themes/colors.rasi
+    ln -sf ~/.config/polybar/colors.$mode.ini       ~/.config/polybar/colors.ini
+    ln -sf ~/.config/dunst/dunstrc.$mode            ~/.config/dunst/dunstrc
+    # write i3 config, comment old theme define and uncomment another
     sed -i -e 's/^#client/@client/' -e 's/^client/\#client/' -e 's/^@client/client/' ~/.config/i3/config
-    echo $LIGHT > $THEME_FILE
-    feh --no-fehbg --bg-fill ~/.config/i3/Ryan.jpg
-else
-    ln -sf ~/.config/polybar/colors.dark.ini ~/.config/polybar/colors.ini
-    ln -sf ~/.config/dunst/dunstrc.dark ~/.config/dunst/dunstrc
-    ln -sf ~/.config/rofi/themes/colors.dark.rasi ~/.config/rofi/themes/colors.rasi
-    sed -i -e 's/^#client/@client/' -e 's/^client/\#client/' -e 's/^@client/client/' ~/.config/i3/config
-    feh --no-fehbg --bg-fill ~/.config/i3/MrLee.jpg
-    echo $DARK > $THEME_FILE
+
+    switch-wallpaper $mode
+    switch-global-theme $mode
+    switch-konsole-theme $mode
+
+    pkill dunst
+    i3-msg restart
+    dunst &
+    notify-send "i3' 'Theme switched to $mode"
+}
+
+switch-wallpaper() {
+    if [ "$1" = "$DARK" ]; then
+        feh --no-fehbg --bg-fill ~/.config/i3/MrLee.jpg
+    else
+        feh --no-fehbg --bg-fill ~/.config/i3/Ryan.jpg
+    fi
+}
+
+switch-konsole-theme() {
+    if [ "$1" = "$DARK" ];then
+        THEME='Gruvbox'
+    else
+        THEME='SolarizedLight'
+    fi
+
+    # find all openned pts by konsole
+    for i in /dev/pts/*; do
+        if test -c $i; then
+            # see /usr/bin/konsoleprofile
+            printf "\033]50;colors=$THEME\a" > $i
+        fi
+    done
+}
+
+switch-global-theme() {
+    if [ "$1" = "$DARK" ];then
+        THEME='Qogir-dark'
+    else
+        THEME='Qogir'
+    fi
+
+    kvantummanager --set $THEME
+    gsettings set org.gnome.desktop.interface gtk-theme $THEME
+    gsettings set org.gnome.desktop.interface icon-theme $THEME
+}
+
+# TODO: switch vim colorscheme
+# TODO: switch chrome color
+
+if [ "$cmd" = "toggle" ]; then
+    if [ "$current_mode" = "$LIGHT" ];then
+        switch-theme $DARK
+    else
+        switch-theme $LIGHT
+    fi
+elif [ "$1" = "reset" ]; then
+    switch-wallpaper $current_mode
 fi
-
-pkill dunst
-i3-msg restart
-dunst &
 
