68bb023e1acfca2e68dea01b25ea5064ac7edef9
ZuoXinyu <iamzuoxinyu@gmail.com>
2020-10-02 00:12:51 +0800

    install.sh

---
+++ .install.sh
@@ -0,0 +1,11 @@
+#!bash
+
+# arch packages
+sudo pacman -S zsh vim git tig neomutt tmux ripgrep most bat htop ranger fzf lsd
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# syntax highlight
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# power10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
