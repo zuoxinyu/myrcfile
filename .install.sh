1674bf5301a6536a89fc876bdc89f2c4e27fce89
zuoxinyu <iamzuoxinyu@gmail.com>
2020-12-30 16:22:20 +0800

    rm nvim coc settings

--- .install.sh
+++ .install.sh
@@ -10,3 +10,5 @@ git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${Z
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # install tpm
 git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# nvim coc settings
+ln -sf $HOME/.vim/coc-settings.json .config/nvim/coc-settings.json
